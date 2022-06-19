a30a0a09ab15fafe4bb3294a23f9a4e1d67b9435
kazukiUshijima <info@kfs214.net>
2022-06-19 09:30:40 +0900

    feat: 環境変数読み込み・インスタンス生成

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello TObiIro!!");
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+
+const boltApp = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_TOKEN,
+});
+
+console.log(
+  "process.env.SLACK_SIGNING_SECRET",
+  process.env.SLACK_SIGNING_SECRET
+);
